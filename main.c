0a7158ab5dd51f6ed77000c7d2753cde0969f9be
Kyaw-Min-Thant <kyawminthant5191@gmail.com>
2023-03-14 09:40:19 +0630

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+
+#include "stdio.h"
+#include "bank.h"
+#include "time.h"
+#include "stdlib.h"
+
+
+int main(){
+    space_counter();
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+
+    return 0;
+}
